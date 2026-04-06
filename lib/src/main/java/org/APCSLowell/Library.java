05ee9f95f8e36e056e3262fc5c7776e24e31e768
mrchanr <42151767+mrchanr@users.noreply.github.com>
2026-04-06 08:23:43 -0700

    Add OnlinePurchaseManager class with methods

--- lib/src/main/java/org/APCSLowell/Library.java
+++ lib/src/main/java/org/APCSLowell/Library.java
@@ -1,4 +1,45 @@
 package org.APCSLowell;
 
-public class Library {
+import java.util.ArrayList;
+public class OnlinePurchaseManager {
+    /** An ArrayList of purchased Gizmo objects,
+     * instantiated in the constructor.
+     */
+    private ArrayList<Gizmo> purchases;
+
+    /** Returns the number of purchased Gizmo objects that are electronic
+     * whose manufacturer is maker, as described in part (a).
+     */
+    public int countElectronicsByMaker(String maker) {
+        /* to be implemented in part (a) */
+        int count = 0;
+        for (Gizmo g : purchases) {
+            if (g.isElectronic()) {
+                if (g.getMaker().equals(maker)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    /** Returns true if any pair of adjacent purchased Gizmo objects are
+     * equivalent, and false otherwise, as described in part (b).
+     */
+    public boolean hasAdjacentEqualPair() {
+        /* to be implemented in part (b) */
+        if (purchases.size() < 2) return false;
+        else {
+            for (int i = 1; i < purchases.size(); i++) {
+                if (purchases.get(i).equals(purchases.get(i-1))) return true;
+            }
+        }
+        return false;
+    }
+    public OnlinePurchaseManager() {
+        purchases = new ArrayList <Gizmo>();
+    }
+    public void add(Gizmo g) {
+        purchases.add(g);
+    }
 }
